15eff4df26cfa50316679eb9579be58fa33ab371
jamiebenstead <jamie.benstead@gmail.com>
2019-11-03 22:15:03 +0000

    Started creating the settings form

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+1.0.1 -
+    Started creating the settings form
+
 1.0.0 - 
     First commit
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    nativeStorage: NativeStorage;
+
+    this.getAmount();
+  }
+
+  init(){
+    console.log("init func");
+  }
+
+  amount;
+  time;
+
+  testFunc(){
+    console.log(this.amount);
+    console.log("testFunc");
+    
+    NativeStorage.setItem('amount', this.amount)
+      .then(
+        () => console.log("Stored: " + this.amount),
+        error => console.error("Error storing amount", error)
+    );
+  }
+
+  getAmount(){
+    NativeStorage.getItem('amount')
+      .then(
+        data => { this.amount = data},
+        error => console.error(error)
+    );
+  }
+
+  logValues(){
+    console.log(this.amount);
+    console.log(this.time);
+  }
 
 }
